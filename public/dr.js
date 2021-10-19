1c6fd33b11c9e2051218ea8500bcbb812ca5f8bc
irfanhsajid <irfanhsajid@gmail.com>
2021-10-19 23:51:37 +0600

    done

---
+++ public/dr.js
@@ -0,0 +1,16 @@
+/*
+https://i.ibb.co/0KK4YSS/closeup-portrait-friendly-smiling-confident-260nw-346634111.jpg
+https://i.ibb.co/Sr5nKq4/doctor-male.png
+https://i.ibb.co/1mr9FD5/doctor-PNG16011.png
+https://i.ibb.co/x1Bjj11/download-1.jpg
+https://i.ibb.co/vPnKLWD/download-2.jpg
+https://i.ibb.co/dbzBwzT/download.jpg
+https://i.ibb.co/Jq0RWns/images-1.jpg
+https://i.ibb.co/v3jtsvt/images-2.jpg
+https://i.ibb.co/Gpcc53x/images-3.jpg
+https://i.ibb.co/RhGkr1x/images-4.jpg
+https://i.ibb.co/2vJkmJF/images-6.jpg
+
+
+
+*/
--- src/Components/Pages/Doctors/Doctors.js
+++ src/Components/Pages/Doctors/Doctors.js
@@ -1,11 +1,41 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import './Doctors.css';
 const Doctors = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('./doctors.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
     return (
-        <div id="doctors">
-            <div className="text-danger text-center bg-dark p-5">
-                <h1>this is DOCTOR'S Page</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, dignissimos!</p>
+        <div className="container mb-5">
+            <div className="text-center my-4">
+                <h1 className="abt-title">Well qualified Doctors Here</h1>
+                <p>All the doctors are well qualified and specialist in the field. Lorem ipsum dolor sit amet. Feel Free to visit here and apointment Here<br />
+                    <strong>Happy Online Education</strong>
+                </p>
+                <p className="border-bottom  border-2 w-50 d-flex mx-auto"></p>
+            </div>
+            <div className="row">
+                {
+                    services.map(service =>
+
+
+                        <div className="col-12 col-md-4 g-4">
+                            <div className="card h-100 card-container">
+                                <img src={service.img} alt="services img" className="card-img" />
+                                <div className="card-body">
+                                    <h2 className="card-title">{service.name}</h2>
+                                    <span className="card-text">
+                                        <strong>Specialist in : {service.specialist}</strong>
+                                        <p>From : {service.education}</p>
+                                    </span>
+                                    <button className="border-0 rounded-2 px-3 py-1 enroll-btn">Appoint Him</button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
         </div>
     );
